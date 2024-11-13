ad3aa34f6e81a326c5feca93738d8605e38c000a
rrongxie <soniaxie5@gmail.com>
2024-11-12 19:30:25 -0600

    1:20:30

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,10 +3,38 @@ import styles from "./about.module.css"
 
 const aboutPage = () => {
     return(
-        <div>
-            <div className={styles.imgContainer}>
-            {/* <Image src="/about.png" alt="" width={500} height={500}/> */}
-            <Image src="https://images.pexels.com/photos/29130848/pexels-photo-29130848/free-photo-of-serene-pine-forest-with-ocean-view.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill/>
+        <div className={styles.container}>
+           <div className={styles.textContainer}>
+            <h2 className={styles.subtitle}>About Agency</h2>
+            <h1 className={styles.title}>
+                We create digital ideas.
+            </h1>
+            <p className={styles.desc}>
+                alkjs slkfjansk kslfl.
+                sjl dshl.
+            </p>
+            <div className={styles.boxes}>
+                <div className={styles.box}>
+                    <h1>10 k+</h1>
+                    <p>year of experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h1>10 k+</h1>
+                    <p>year of experience</p>
+                </div>
+                <div className={styles.box}>
+                    <h1>10 k+</h1>
+                    <p>year of experience</p>
+                </div>
+            </div>
+
+           </div>
+           <div className={styles.imgContainer}>
+            <Image 
+                src="/about.png"
+                alt="About Image"
+                fill
+                className={styles.img}/>
             </div>
         </div>
     )
